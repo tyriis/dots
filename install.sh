e21c36a3025250bdf46061e36fc0086cef0b63d2
Nils <tyriis@users.noreply.github.com>
2026-09-13 17:01:15 +0200

    chore(install): track additional installed packages

--- install.sh
+++ install.sh
@@ -101,6 +101,7 @@ pacman_packages=(
     pipewire pipewire-pulse wireplumber
     polkit-gnome networkmanager nm-connection-editor network-manager-applet
     eza bat ripgrep libfido2
+    7zip btrfs-progs cmake hashcat less mosquitto obsidian tig unzip vim wev wget zed
     neovim code docker docker-compose starship nvidia-container-toolkit openssh intel-ucode ipmitool tcpdump
 )
 
